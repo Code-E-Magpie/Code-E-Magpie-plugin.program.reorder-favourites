6efab8fbef6d7440ce7bbd77d728aa7fb6165fda
Code-E-Magpie <279868713+Code-E-Magpie@users.noreply.github.com>
2026-09-09 16:20:05 +0100

    Add files via upload

--- reorder_favourites.py
+++ reorder_favourites.py
@@ -33,7 +33,7 @@ ADDON_FANART = ADDON.getAddonInfo('fanart')
 ADDON_ICON = ADDON.getAddonInfo('icon')
 ADDON_NAME = ADDON.getAddonInfo('name') # name in addons.xml
 ADDON_VERSION = ADDON.getAddonInfo('version') # version in addons.xml
-FAVOURITES = os.path.join(xbmcvfs.translatePath('special://userdata/'), 'favourites.xml')
+FAVOURITES = os.path.join(xbmcvfs.translatePath('special://userdata/'), 'favourites.xml') # count
 FAVOURITES_RESULT = 'ordfav.result'
 PLUGIN_ID = int(sys.argv[1])
 PLUGIN_URL = sys.argv[0]
@@ -154,7 +154,7 @@ SETTINGS_TEXT = '[CR][CR][CR]%s[CR][CR]Press the OK button in settings to save a
 
 ENVIRONMENT_TEXT = '[CR][CR][CR]%s[CR][CR]Kodi v21.3 Omega apk (Android app) with Confluence skin as default (including default font).[CR]Tablet (1340 x 800 aspect ratio 5:3) running Android 14 using QuickEdit apk (TryItAndSee / LearnAsYouGo iterative development and testing).[CR]Chromecast HD (1280 x 720 aspect ratio 16:9) running Android TV OS version 14 (user testing).[CR]100%% tested and working on Android.[CR]Not tested on other platforms.[CR]Code debugged and reengineered using https://aipy.dev/tools where required (pre 1.6.0).[CR]Code debugged and reengineered using https://stackoverflow.com/ai-assist (1.6.0 onwards).' % ' '.join('DEVELOPMENT ENVIRONMENT')
 
-CHANGELOG_TEXT = '[CR][CR][CR]%s [LIGHT] (newest at the top)[/LIGHT][CR][CR]Version code x.y.z attributes (1.5.0 onwards)[CR]x = major change / y = number of \'>\' menu items / z = minor change[CR][CR]version 3.4.0 (4 menu items & 2 user interface buttons)[CR]- settings reworked to avoid clashes (different names to variables etc.)[CR]- minor changes to ReorderFavourites.xml and TextBox.xml to improve performance and consistency with other add-ons[CR]- minor changes to Log and TextBox functions to improve consistency with other add-ons[CR]- reorder favourites code improved to retrieve more thumbnails[CR][CR]version 2.4.0 (4 menu items & 2 user interface buttons)[CR]- save and exit options removed from menu[CR]- save and exit options added using dialogue boxes[CR][CR]version 1.6.0 (6 menu items & 2 user interface buttons)[CR]- settings created to customise text colours with billions of text colour combinations[CR]- text colour customisation includes text boxes and user interface buttons[CR]- added favourite and interface row count to user interface header[CR]- added dummy button containing full favourite text to user interface[CR]- minor changes to menu text formats to improve consistency with other add-ons[CR]- minor changes to function names to improve consistency with other add-ons[CR]- logs reworked[CR][CR]version 1.5.1 (5 menu items & 2 user interface buttons)[CR]- minor changes to menu text formats to improve consistency with other add-ons[CR][CR]version 1.5.0 (5 menu items & 2 user interface buttons)[CR]- Textbox.xml background image name change[CR]- minor changes to improve consistency with other add-ons[CR][CR]version 1.2.4 (4 menu items for user interface & 2 user interface buttons)[CR]- menu updated with User Information dialogue box (Instructions / Notes / Development / Changelog)[CR]- menu updated with Developer, Name, Version and Addon ID[CR]- user interface ids in xml renumbered[CR]- user interface remote scrolling within borders[CR]- user interface images and layout improved[CR]- variables and functions reworked[CR]- dialogue boxes and logs reworked[CR]- simplified addon.xml content to reduce maintenance[CR][CR]version 1.0.0 (4 menu items for user interface & 2 user interface buttons)[CR]- code from Order Favourites 1.2.3a by doko-desuka (plugin.program.orderfavourites)[CR]- user interface resized to full screen[CR]- improved layout using new images and default image[CR]- visible scrollbar and resized text[CR]- menu and dialogue boxes reworked[CR]- user instructions added to addon.xml[CR]- icon.png changed and fanart.jpg added' % ' '.join('CHANGELOG')
+CHANGELOG_TEXT = '[CR][CR][CR]%s [LIGHT] (newest at the top)[/LIGHT][CR][CR]Version code x.y.z attributes (1.5.0 onwards)[CR]x = major change / y = number of \'>\' menu items / z = minor change[CR][CR]version 2.4.1 (4 menu items & 2 user interface buttons)[CR]- settings reworked to avoid clashes (different names to variables etc.)[CR]- minor changes to ReorderFavourites.xml and TextBox.xml to improve performance and consistency with other add-ons[CR]- minor changes to Log and TextBox functions to improve consistency with other add-ons[CR][CR]version 2.4.0 (4 menu items & 2 user interface buttons)[CR]- save and exit options removed from menu[CR]- save and exit options added using dialogue boxes[CR][CR]version 1.6.0 (6 menu items & 2 user interface buttons)[CR]- settings created to customise text colours with billions of text colour combinations[CR]- text colour customisation includes text boxes and user interface buttons[CR]- added favourite and interface row count to user interface header[CR]- added dummy button containing full favourite text to user interface[CR]- minor changes to menu text formats to improve consistency with other add-ons[CR]- minor changes to function names to improve consistency with other add-ons[CR]- logs reworked[CR][CR]version 1.5.1 (5 menu items & 2 user interface buttons)[CR]- minor changes to menu text formats to improve consistency with other add-ons[CR][CR]version 1.5.0 (5 menu items & 2 user interface buttons)[CR]- Textbox.xml background image name change[CR]- minor changes to improve consistency with other add-ons[CR][CR]version 1.2.4 (4 menu items for user interface & 2 user interface buttons)[CR]- menu updated with User Information dialogue box (Instructions / Notes / Development / Changelog)[CR]- menu updated with Developer, Name, Version and Addon ID[CR]- user interface ids in xml renumbered[CR]- user interface remote scrolling within borders[CR]- user interface images and layout improved[CR]- variables and functions reworked[CR]- dialogue boxes and logs reworked[CR]- simplified addon.xml content to reduce maintenance[CR][CR]version 1.0.0 (4 menu items for user interface & 2 user interface buttons)[CR]- code from Order Favourites 1.2.3a by doko-desuka (plugin.program.orderfavourites)[CR]- user interface resized to full screen[CR]- improved layout using new images and default image[CR]- visible scrollbar and resized text[CR]- menu and dialogue boxes reworked[CR]- user instructions added to addon.xml[CR]- icon.png changed and fanart.jpg added' % ' '.join('CHANGELOG')
 
 User_Information_Text = '[COLOR %s][B]%s[/B][CR][COLOR %s][LIGHT](Instructions / Notes / Settings / Development Environment / Changelog)[/LIGHT][/COLOR][/COLOR][CR][CR][COLOR %s]%s[/COLOR]' % (TEXT_ITEM, ' '.join('USER INFORMATION'), TEXT_VALUE, TEXT_GENERAL, (INSTRUCTIONS_TEXT + NOTES_TEXT + SETTINGS_TEXT + ENVIRONMENT_TEXT + CHANGELOG_TEXT))
 
@@ -358,89 +358,46 @@ def Count_Favourites(file_path):
 
 def Data_Generator_Favourites():
 
-	try:
-		file = xbmcvfs.File(FAVOURITES)
-		contents = file.read()
-		file.close()
+	decode_string = lambda val: val
 
-	except Exception:
-		return 
+	file = xbmcvfs.File(FAVOURITES)
+	contents = decode_string(file.read())
+	file.close()
 
-	try:
-		root = ET.fromstring(contents)
+	namePattern = re.compile('name="([^"]+)')
+	thumbPattern = re.compile('thumb="([^"]+)')
 
-	except Exception:
-		return
+	for entryMatch in re.finditer('(<favourite\s[^<]+</favourite>)', contents):
+		entry = entryMatch.group(1)
 
-	for favourite in root.findall('.//favourite'):
-		name = favourite.get('name') or ''
-		name = html.unescape(name)
+		match = namePattern.search(entry)
+		name = html.unescape(match.group(1)) if match else ''
 
-		thumb_attr = favourite.get('thumb') or ''
-		thumb_attr = html.unescape(thumb_attr).strip()
+		match = thumbPattern.search(entry)
 
-		thumb_result = ''
+		if match:
+			thumb = html.unescape(match.group(1))
+			cacheFilename = xbmc.getCacheThumbName(thumb)
 
-		if thumb_attr:
-			# Some thumb URLs are virtual (plugin://, image://, resource://) translatePath may help for resource:// and file://
-			try:
-				translated = xbmcvfs.translatePath(thumb_attr)
-			except Exception:
-				translated = thumb_attr
-
-			# Get cache filename
-			try:
-				cacheFilename = xbmc.getCacheThumbName(thumb_attr)
-			except Exception:
-				cacheFilename = ''
-
-			# If getCacheThumbName returns something and not a placeholder, check for the actual file
-			if cacheFilename and 'ffffffff' not in cacheFilename:
-				# Construct the cache path using Kodi's thumbnails pattern
-				# getCacheThumbName returns something like 'a/abcdef012345.tbn'
-				# Resolve to Thumbnails folder
-				thumbs_path = THUMBNAILS_FORMAT.format(folder=cacheFilename[0], file=cacheFilename)
-				thumbs_path = xbmcvfs.translatePath(thumbs_path)
-
-				# Use it if it exist and try common extension replacements (.jpg/.png)
-				if xbmcvfs.exists(thumbs_path):
-					thumb_result = thumbs_path
-				else:
-					# try replacing .tbn with common extensions if original url had one
-					if thumb_attr.lower().endswith('.jpg'):
-						alt = thumbs_path.replace('.tbn', '.jpg', 1)
-						if xbmcvfs.exists(alt):
-							thumb_result = alt
-					elif thumb_attr.lower().endswith('.png'):
-						alt = thumbs_path.replace('.tbn', '.png', 1)
-						if xbmcvfs.exists(alt):
-							thumb_result = alt
-
-				# If the cache file is in a different place, try the raw cache name in Thumbnails root
-				if not thumb_result:
-					# try special://Thumbnails/<firstchar>/<cacheFilename>
-					try_root = xbmcvfs.translatePath('special://Thumbnails/{0}/{1}'.format(cacheFilename[0], cacheFilename))
-					if xbmcvfs.exists(try_root):
-						thumb_result = try_root
-
-			# If cache file won't map use the translated path if it exists
-			if not thumb_result and translated and xbmcvfs.exists(translated):
-				thumb_result = translated
-
-			# Fall back to the original thumb_attr if previous doesn't work
-			if not thumb_result:
-				thumb_result = thumb_attr
+			if 'ffffffff' not in cacheFilename:
+				if '.jpg' in thumb:
+					cacheFilename = cacheFilename.replace('.tbn', '.jpg', 1)
+				if '.png' in thumb:
+					cacheFilename = cacheFilename.replace('.tbn', '.png', 1)
+				thumb = THUMBNAILS_FORMAT.format(folder=cacheFilename[0], file=cacheFilename)
 
 		else:
-			thumb_result = ''
+			thumb = ''
 
-		yield name, thumb_result, ET.tostring(favourite, encoding='unicode')
+		# Yield a 3-tuple of name, thumb-url and the original favourite.
+		yield name, thumb, entry
 
 # ============================================================
 # FUNCTION: Save_Favourites
 # ============================================================
 
 def Save_Favourites(xmlText):
+
 	if not xmlText:
 		return False
 
@@ -450,7 +407,7 @@ def Save_Favourites(xmlText):
 		file.close()
 
 	except Exception as e:
-		Log(Log_Title + Favourites + 'Save Favourites: %s' % str(e), xbmc.LOGERROR)
+		Log(Log_Title + Favourites + 'Save Favourites: exception[CR]%s' % str(e), xbmc.LOGERROR)
 
 	return True
 
@@ -510,7 +467,7 @@ def Reorder_Favourites():
 		Window_Property_Set(FAVOURITES_RESULT, result)
 
 	except Exception as e:
-		Log(Log_Title + Favourites + 'User Interface: %s' % str(e), xbmc.LOGERROR)
+		Log(Log_Title + Favourites + 'User Interface: exception[CR]%s' % str(e), xbmc.LOGERROR)
 
 		Window_Property_Clear(FAVOURITES_RESULT)
 
@@ -542,7 +499,7 @@ def Save_Exit():
 		xbmc.executebuiltin('Action(Back)')
 
 	except Exception as e:
-		Log(Log_Title + Favourites + 'Save + Exit: %s' % str(e), xbmc.LOGERROR)
+		Log(Log_Title + Favourites + 'Save + Exit: exception[CR]%s' % str(e), xbmc.LOGERROR)
 
 	Log(Log_Title + Favourites + '[COLOR %s][LIGHT]Finished (Save + Exit)[/LIGHT][/COLOR]' % TEXT_DARK, xbmc.LOGINFO)
 
@@ -563,7 +520,7 @@ def Save_Reload():
 			xbmc.executebuiltin('LoadProfile(%s)' % xbmc.getInfoLabel('System.ProfileName'))
 
 	except Exception as e:
-		Log(Log_Title + Favourites + 'Save + Reload: %s' % str(e), xbmc.LOGERROR)
+		Log(Log_Title + Favourites + 'Save + Reload: exception[CR]%s' % str(e), xbmc.LOGERROR)
 
 	Log(Log_Title + Favourites + '[COLOR %s][LIGHT]Finished (Save + Reload)[/LIGHT][/COLOR]' % TEXT_DARK, xbmc.LOGINFO)
 
